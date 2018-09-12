1b3cfee20c17e6b247df0ba06d68bf9fb506e111
Ivan Neves Silveira Bandeira de Souza <insbs@windows.cin.ufpe.br>
2018-09-12 08:38:36 -0300

    --all

---
+++ E.D/HashTables/main.cpp
@@ -0,0 +1,9 @@
+#include "../ListaQueueStack/List.cpp"
+#include "HashTable.cpp"
+#include <iostream>
+
+int main()
+{
+    List<HashNode<int, int>>* arr = new List<HashNode<int, int>>[20];
+    return 0;
+}
