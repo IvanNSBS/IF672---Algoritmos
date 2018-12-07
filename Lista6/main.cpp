43d20cf4de26e05250c79631f2a2c86aa72c0b15
Ivan Neves Silveira Bandeira de Souza <insbs@ug1c36.windows.cin.ufpe.br>
2018-12-07 08:49:38 -0300

    Lista 6 qse feita

--- Lista6/main.cpp
+++ Lista6/main.cpp
@@ -1,104 +1,92 @@
 #include <stdio.h> 
 #include <limits.h> 
   
-// Number of vertices  
-// in the graph 
-#define V 9 
-
 struct Graph
 {
 public:
     int num_vertices;
-    int **adjList;// = new int[num_vertices][num_vertices];
-    bool biDirectional;
+    int **adj_list;// = new int[num_vertices][num_vertices];
+    int **path_list;
 public:
     Graph(){}
-    Graph(const int &v, const bool &bd) : num_vertices(v), biDirectional(bd){ 
-        adjList = new int*[v];
+    Graph(const int &v) : num_vertices(v)
+    { 
+        adj_list = new int*[v];
         for(int i = 0; i < v; i++)
-            adjList[i] = new int[v];
+            adj_list[i] = new int[v];
+
 
         for(int i = 0; i < v; i++)
             for(int j = 0; j < v; j++)
-                adjList[i][j] = 0;
+                adj_list[i][j] = 0;
     }
 
-    CreateVertice(int src, int dest, int weight){
-        adjList[src][dest] = weight;
-        if(biDirectional)
-            adjList[dest][src] = weight;
+    void add_edge(int src, int dest, int weight){
+        adj_list[src][dest] = weight;
+        adj_list[dest][src] = weight;
     }
+    
+    int min_distance(int dist[], bool visited[]) 
+    { 
+        // Initialize min value 
+        int min = INT_MAX, min_index; 
+    
+        for (int v = 0; v < num_vertices; v++) 
+            if (visited[v] == false && dist[v] <= min) 
+                min = dist[v], min_index = v; 
+    
+        return min_index; 
+    } 
+
+    void print_path(int parent[], int j) 
+    { 
+        if (parent[j] == - 1) 
+            return; 
+    
+        print_path(parent, parent[j]); 
+    
+        printf("%d ", j); 
+    } 
+    
+    void dijkstra(int src, int dest) 
+    { 
+        int dist[num_vertices];  
+        bool visited[num_vertices];
+        int parent[num_vertices]; 
+    
+        for (int i = 0; i < num_vertices; i++) 
+        { 
+            parent[0] = -1; 
+            dist[i] = INT_MAX; 
+            visited[i] = false; 
+        } 
+
+        dist[src] = 0;  
+        for (int count = 0; count < num_vertices - 1; count++) 
+        {  
+            int u = min_distance(dist, visited); 
+            visited[u] = true; 
+            for (int v = 0; v < num_vertices; v++) 
+                if (!visited[v] && adj_list[u][v] && 
+                    dist[u] + adj_list[u][v] < dist[v]) 
+                { 
+                    parent[v] = u; 
+                    dist[v] = dist[u] + adj_list[u][v]; 
+                }  
+        }
+        printf("%d ", src);
+        print_path(parent, dest);
+        printf("\n"); 
+    } 
 };
-  
-int minDistance(int dist[], bool sptSet[]) 
-{ 
-    // Initialize min value 
-    int min = INT_MAX, min_index; 
-  
-    for (int v = 0; v < V; v++) 
-        if (sptSet[v] == false && 
-                   dist[v] <= min) 
-            min = dist[v], min_index = v; 
-  
-    return min_index; 
-} 
 
-void printPath(int parent[], int j) 
-{ 
-    if (parent[j] == - 1) 
-        return; 
-  
-    printPath(parent, parent[j]); 
-  
-    printf("%d ", j); 
-} 
 
-int printSolution(int dist[], int n, int parent[]) 
-{ 
-    int src = 0; 
-    printf("Vertex\t Distance\tPath"); 
-    for (int i = 1; i < V; i++) 
-    { 
-        printf("\n%d -> %d \t\t %d\t\t%d ", 
-                      src, i, dist[i], src); 
-        printPath(parent, i); 
-    } 
-} 
-  
-void dijkstra(int graph[V][V], int src) 
-{ 
-    int dist[V];  
-    bool sptSet[V]; 
-    int parent[V]; 
-  
-    for (int i = 0; i < V; i++) 
-    { 
-        parent[0] = -1; 
-        dist[i] = INT_MAX; 
-        sptSet[i] = false; 
-    } 
-
-    dist[src] = 0;  
-    for (int count = 0; count < V - 1; count++) 
-    {  
-        int u = minDistance(dist, sptSet); 
-        sptSet[u] = true; 
-        for (int v = 0; v < V; v++) 
-            if (!sptSet[v] && graph[u][v] && 
-                dist[u] + graph[u][v] < dist[v]) 
-            { 
-                parent[v] = u; 
-                dist[v] = dist[u] + graph[u][v]; 
-            }  
-    } 
-    printSolution(dist, V, parent); 
-} 
 // Driver Code 
 int main() 
 { 
     //  Let us create the example 
     // graph discussed above 
-    int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0}, 
+    int graph[9][9] = {{0, 4, 0, 0, 0, 0, 0, 8, 0}, 
                        {4, 0, 8, 0, 0, 0, 0, 11, 0}, 
                        {0, 8, 0, 7, 0, 4, 0, 0, 2}, 
                        {0, 0, 7, 0, 9, 14, 0, 0, 0}, 
@@ -106,9 +94,55 @@ int main()
                        {0, 0, 4, 0, 10, 0, 2, 0, 0}, 
                        {0, 0, 0, 14, 0, 2, 0, 1, 6}, 
                        {8, 11, 0, 0, 0, 0, 1, 0, 7}, 
-                       {0, 0, 2, 0, 0, 0, 6, 7, 0} 
-                    }; 
+                       {0, 0, 2, 0, 0, 0, 6, 7, 0}}; 
   
-    dijkstra(graph, 0); 
+    Graph *a = new Graph(9);
+    for(int i = 0; i < 9; ++i)
+        for(int j = 0; j < 9; ++j)
+            a->add_edge(i,j, graph[i][j]);
+
+    a->dijkstra(0, 9); 
     return 0; 
 } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int printSolution(int dist[], int n, int parent[]) 
+// { 
+//     int src = 0; 
+//     printf("Vertex\t Distance\tPath"); 
+//     for (int i = 1; i < V; i++) 
+//     { 
+//         printf("\n%d -> %d \t\t %d\t\t%d ", 
+//                       src, i, dist[i], src); 
+//         print_path(parent, i); 
+//     }
+//     printf("\n"); 
+// } 
